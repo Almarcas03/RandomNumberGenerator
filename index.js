e685847b132fba2abbcd8da11712b33bacbe04f5
BuildTools <almarcas3@gmail.com>
2020-10-11 14:04:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function st() {
+    var min = document.getElementById("min").value;
+    var max = document.getElementById("max").value;
+
+    RNG(min,max)
+
+}
+
+function RNG(min,max){
+    document.getElementById("result").innerHTML = (Math.floor(Math.random() * (max - min) + min));
+}
