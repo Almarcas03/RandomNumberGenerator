14a764dc398ff146a562f8dfdaac701c6db92080
Almarcas03 <59236161+Almarcas03@users.noreply.github.com>
2021-11-22 15:00:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ function st() {
 
 }
 
+function dmg () {
+    var atq = document.getElementById("atq").value;
+    var def = document.getElementById("def").value;
+    var pot = document.getElementById("pot").value;
+    function atk_dmg (atq, def, pot)
+}
+function prec () {
+    var pre = document.getElementById("pre").value;
+    var eva = document.getElementById("eva").value;
+    var acc = document.getElementById("acc").value;
+
+    function atk_prec (pre, eva, acc)
+}
+
 function RNG(min,max){
     document.getElementById("result").innerHTML = Math.floor((Math.random() * max) + min)+1;
+}
+
+function atk_dmg (atq, def, pot){
+    document.getElementById("dmg").innerHTML = (atq + pot)/def;
+}
+
+function atk_prec ( pre, eva, acc) {
+    document.getElementById("prec").innerHTML = "dc(" + ((pre - eva)+acc)*0.9 + ")";
 }
